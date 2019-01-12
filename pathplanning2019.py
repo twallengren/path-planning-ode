94d96670fe030658238d58403776342be52f065f
Toren Wallengren <toren.wallengren@addepar.com>
2019-01-12 02:32:32 -0700

    can now update and plot solution

--- pathplanning2019.py
+++ pathplanning2019.py
@@ -10,7 +10,7 @@ https://www.youtube.com/watch?v=fNBrIngCJp8&t=9s
 # Import necessary libraries
 import numpy as np
 import sympy as sp
-import utils
+import matplotlib.pyplot as plt
 
 ################################################################################
 ################################################################################
@@ -40,7 +40,7 @@ class PathPlanningODE():
         self.Path = Path(self.Rover, NUM_OF_STEPS)
 
         # Create ODE object to solve
-        self.ode = ODE()
+        self.Ode = ODE()
 
     def create_obstacle(self,
                        coordinate = None,
@@ -58,42 +58,69 @@ class PathPlanningODE():
         self.obstacle_list.append(obstacle)
 
         # Add obstacle to cost function
-        self.ode.add_to_cost(obstacle)
+        self.Ode.add_to_cost(obstacle)
 
     def update_path(self):
 
         # Get step size (change in time for each point in parametrized path)
-        self.step_size = self.Path.time_list[1]
+        self.STEP_SIZE = self.Path.time_list[1]
 
         # Create solution vector with 2*NUM_OF_STEPS rows and 1 column
         # First half are path x-coordinates, second half are path y-coordinates
-        self.next_path = np.concatenate((self.Path.path[0][1:self.NUM_OF_STEPS+1],
+        old_path = np.concatenate((self.Path.path[0][1:self.NUM_OF_STEPS+1],
                                          self.Path.path[1][1:self.NUM_OF_STEPS+1]),
-                                         axis=None)
+                                         axis=None).reshape(-1, 1)
 
         # Initialize array for calculating ode delta
-        self.delta = np.zeros([2*self.NUM_OF_STEPS,1])
+        delta = np.zeros([2*self.NUM_OF_STEPS,1])
 
         # Define input to calculate delta at beginning of path
-        x0, x1, x2 = self.Rover.starting_coordinate[0], self.next_path[0], self.next_path[1]
-        y0, y1, y2 = self.Rover.starting_coordinate[1], self.next_path[self.NUM_OF_STEPS], self.next_path[self.NUM_OF_STEPS+1]
+        x0, x1, x2 = self.Rover.starting_coordinate[0], old_path[0], old_path[1]
+        y0, y1, y2 = self.Rover.starting_coordinate[1], old_path[self.NUM_OF_STEPS], old_path[self.NUM_OF_STEPS+1]
 
         # Calculate first set of ode deltas
-        self.delta[0], self.delta[self.NUM_OF_STEPS] = self.ode.ode_delta(x0, x1, x2, y0, y1, y2, self.step_size)
+        delta[0], delta[self.NUM_OF_STEPS] = self.Ode.ode_delta(x0, x1, x2, y0, y1, y2, self.STEP_SIZE)
 
         # Loop to calculate intermediate ode deltas
         for i in range(1,self.NUM_OF_STEPS-1):
-            self.delta[i], self.delta[self.NUM_OF_STEPS+i] = self.ode.ode_delta(self.next_path[i-1],self.next_path[i],self.next_path[i+1],self.next_path[self.NUM_OF_STEPS-1+i],self.next_path[self.NUM_OF_STEPS+i],self.next_path[self.NUM_OF_STEPS+1+i],self.step_size)
+            delta[i], delta[self.NUM_OF_STEPS+i] = self.Ode.ode_delta(old_path[i-1],old_path[i],old_path[i+1],old_path[self.NUM_OF_STEPS-1+i],old_path[self.NUM_OF_STEPS+i],old_path[self.NUM_OF_STEPS+1+i],self.STEP_SIZE)
 
 
         # Define input to calculate delta at beginning of path
-        x0, x1, x2 = self.next_path[self.NUM_OF_STEPS-2], self.next_path[self.NUM_OF_STEPS-1], self.Rover.ending_coordinate[0]
-        y0, y1, y2 = self.next_path[2*self.NUM_OF_STEPS-2], self.next_path[2*self.NUM_OF_STEPS-1], self.Rover.ending_coordinate[1]
+        x0, x1, x2 = old_path[self.NUM_OF_STEPS-2], old_path[self.NUM_OF_STEPS-1], self.Rover.ending_coordinate[0]
+        y0, y1, y2 = old_path[2*self.NUM_OF_STEPS-2], old_path[2*self.NUM_OF_STEPS-1], self.Rover.ending_coordinate[1]
         
         # Calculate final ODE delta
-        self.delta[self.NUM_OF_STEPS-1], self.delta[2*self.NUM_OF_STEPS-1] = self.ode.ode_delta(x0, x1, x2, y0, y1, y2, self.step_size)
+        delta[self.NUM_OF_STEPS-1], delta[2*self.NUM_OF_STEPS-1] = self.Ode.ode_delta(x0, x1, x2, y0, y1, y2, self.STEP_SIZE)
 
-        print(self.delta)
+        # Compute jacobian
+        jacobian = self.Ode.jacobian(self.Rover.starting_coordinate, self.Rover.ending_coordinate, old_path, self.NUM_OF_STEPS, self.STEP_SIZE)
+
+        # Get inverse of jacobian
+        jacobian_inverse = np.linalg.inv(jacobian)
+
+        # Create new path using Newton's Method
+        new_path = old_path - np.dot(jacobian_inverse, delta)
+
+        # Update Path object
+        self.Path.update_path(new_path)
+
+    def show_solution(self):
+
+        # Plot rover
+        plt.plot(self.Rover.current_coordinate[0], self.Rover.current_coordinate[1], 'ro')
+
+        # Plot obstacles
+        for obstacle in self.obstacle_list:
+
+            plt.plot(obstacle.coordinate[0], obstacle.coordinate[1], 'x')
+
+        # Plot solution
+        plt.plot(self.Path.path[0], self.Path.path[1])
+
+        # Show plot
+        plt.show()
+        
         
 ################################################################################
 ################################################################################
@@ -157,11 +184,11 @@ class Path():
                  ):
 
         # Get rover coordinates
-        startxcoord, startycoord = Rover.starting_coordinate
-        endxcoord, endycoord = Rover.ending_coordinate
+        self.startxcoord, self.startycoord = Rover.starting_coordinate
+        self.endxcoord, self.endycoord = Rover.ending_coordinate
 
         # Initialize guess path function (builds a straight line parametrized by t)
-        self.path_func = lambda t: (startxcoord * (1-t) + endxcoord * t, startycoord * (1-t) + endycoord * t)
+        self.path_func = lambda t: (self.startxcoord * (1-t) + self.endxcoord * t, self.startycoord * (1-t) + self.endycoord * t)
 
         # Set time list with defined number of steps
         self.time_list = np.linspace(0, 1, NUM_OF_STEPS + 2)
@@ -171,6 +198,9 @@ class Path():
 
         # Initialize path modification count (for use in __repr__ method)
         self.mod_count = 0
+
+        # Set NUM_OF_STEPS on class variabls
+        self.NUM_OF_STEPS = NUM_OF_STEPS
 
     def __repr__(self):
 
@@ -185,6 +215,21 @@ class Path():
 
         # Set new guess path with t ranging from 0 to 1
         self.path = self.path_func(np.linspace(0, 1, NUM_OF_STEPS))
+
+    def update_path(self, new_path):
+
+        # Break new_path into x & y and append start + end coordinates
+        solx = np.zeros([self.NUM_OF_STEPS+2,1])
+        solx[0] = self.startxcoord
+        solx[1:self.NUM_OF_STEPS+1] = new_path[0:int(len(new_path)/2)]
+        solx[self.NUM_OF_STEPS+1] = self.endxcoord
+
+        soly = np.zeros([self.NUM_OF_STEPS+2,1])
+        soly[0] = self.startycoord
+        soly[1:self.NUM_OF_STEPS+1] = new_path[int(len(new_path)/2):int(len(new_path))]
+        soly[self.NUM_OF_STEPS+1] = self.endycoord
+
+        self.path = solx, soly
                  
 ################################################################################
 ################################################################################
@@ -265,3 +310,93 @@ class ODE():
         yout = (y2 - 2*y1 + y0)/h - self.G(x1,y1,(x2-x0)/(2*h),(y2-y0)/(2*h))
 
         return xout, yout
+
+    def jacobian(self, ALPHA, BETA, solmat, N, h):
+
+        # Initialize matrix
+        jac = np.zeros([2*N,2*N])
+
+        # Populate the first row of M11 (upper left square of jac)
+        jac[0,0] = -2/h**2 - self.Fx(solmat[0],solmat[N],
+                            (solmat[1]-ALPHA[0])/(2*h),(solmat[N+1]-ALPHA[1])/(2*h))
+        jac[0,1] = 1/h**2 - (1/(2*h))*self.Fxp(solmat[0],solmat[N],
+                            (solmat[1]-ALPHA[0])/(2*h),(solmat[N+1]-ALPHA[1])/(2*h))
+
+        # Populate the first row of M21 (lower left square of jac)
+        jac[N,0] = -self.Gx(solmat[0],solmat[N],
+                    (solmat[1]-ALPHA[0])/(2*h),(solmat[N+1]-ALPHA[1])/(2*h))
+        jac[N,1] = (-1/(2*h))*self.Gxp(solmat[0],solmat[N],
+                            (solmat[1]-ALPHA[0])/(2*h),(solmat[N+1]-ALPHA[1])/(2*h))
+
+        # Populate the first row of M12 (upper right square of jac)
+        jac[0,N] = -self.Fy(solmat[0],solmat[N],
+                            (solmat[1]-ALPHA[0])/(2*h),(solmat[N+1]-ALPHA[1])/(2*h))
+        jac[0,N+1] = (-1/(2*h))*self.Fyp(solmat[0],solmat[N],
+                            (solmat[1]-ALPHA[0])/(2*h),(solmat[N+1]-ALPHA[1])/(2*h))
+
+        # Populate the first row of M22 (lower right square of jac)
+        jac[N,N] = -2/h**2 - self.Gy(solmat[0],solmat[N],
+                            (solmat[1]-ALPHA[0])/(2*h),(solmat[N+1]-ALPHA[1])/(2*h))
+        jac[N,N+1] = 1/h**2 - (1/(2*h))*self.Gyp(solmat[0],solmat[N],
+                            (solmat[1]-ALPHA[0])/(2*h),(solmat[N+1]-ALPHA[1])/(2*h))
+
+        # Loop to populate intermediate values of jac
+        for i in range(1,N-1):
+
+            # Populate intermediate values of M11
+            jac[i,i-1] = 1/h**2 + (1/(2*h))*self.Fxp(solmat[i],solmat[N+i],
+                            (solmat[i+1]-solmat[i-1])/(2*h),(solmat[N+1+i]-solmat[N-1+i])/(2*h))
+            jac[i,i] = -2/h**2 - self.Fx(solmat[i],solmat[N+i],
+                            (solmat[i+1]-solmat[i-1])/(2*h),(solmat[N+1+i]-solmat[N-1+i])/(2*h))
+            jac[i,i+1] = 1/h**2 - (1/(2*h))*self.Fxp(solmat[i],solmat[N+i],
+                            (solmat[i+1]-solmat[i-1])/(2*h),(solmat[N+1+i]-solmat[N-1+i])/(2*h))
+
+            # Populate intermediate values of M21
+            jac[N+i,i-1] = (1/(2*h))*self.Gxp(solmat[i],solmat[N+i],
+                            (solmat[i+1]-solmat[i-1])/(2*h),(solmat[N+1+i]-solmat[N-1+i])/(2*h))
+            jac[N+i,i] = -self.Gx(solmat[i],solmat[N+i],
+                            (solmat[i+1]-solmat[i-1])/(2*h),(solmat[N+1+i]-solmat[N-1+i])/(2*h))
+            jac[N+i,i+1] = (-1/(2*h))*self.Gxp(solmat[i],solmat[N+i],
+                            (solmat[i+1]-solmat[i-1])/(2*h),(solmat[N+1+i]-solmat[N-1+i])/(2*h))
+
+            # Populate intermediate values of M12
+            jac[i,N+i-1] = (1/(2*h))*self.Fyp(solmat[i],solmat[N+i],
+                            (solmat[i+1]-solmat[i-1])/(2*h),(solmat[N+1+i]-solmat[N-1+i])/(2*h))
+            jac[i,N+i] = -self.Fy(solmat[i],solmat[N+i],
+                            (solmat[i+1]-solmat[i-1])/(2*h),(solmat[N+1+i]-solmat[N-1+i])/(2*h))
+            jac[i,N+i+1] = (-1/(2*h))*self.Fyp(solmat[i],solmat[N+i],
+                            (solmat[i+1]-solmat[i-1])/(2*h),(solmat[N+1+i]-solmat[N-1+i])/(2*h))
+
+            # Populate intermediate values of M22
+            jac[N+i,N+i-1] = 1/h**2 + (1/(2*h))*self.Gyp(solmat[i],solmat[N+i],
+                            (solmat[i+1]-solmat[i-1])/(2*h),(solmat[N+1+i]-solmat[N-1+i])/(2*h))
+            jac[N+i,N+i] = -2/h**2 - self.Gy(solmat[i],solmat[N+i],
+                            (solmat[i+1]-solmat[i-1])/(2*h),(solmat[N+1+i]-solmat[N-1+i])/(2*h))
+            jac[N+i,N+i+1] = 1/h**2 - (1/(2*h))*self.Gyp(solmat[i],solmat[N+i],
+                            (solmat[i+1]-solmat[i-1])/(2*h),(solmat[N+1+i]-solmat[N-1+i])/(2*h))
+
+        # Populate final values of M11
+        jac[N-1,N-2] = 1/h**2 + (1/(2*h))*self.Fxp(solmat[N-1],solmat[2*N-1],
+                            (BETA[0] - solmat[N-2])/(2*h),(BETA[1] - solmat[2*N-2])/(2*h))
+        jac[N-1,N-1] = -2/h**2 - self.Fx(solmat[N-1],solmat[2*N-1],
+                            (BETA[0] - solmat[N-2])/(2*h),(BETA[1] - solmat[2*N-2])/(2*h))
+
+        # Populate final values of M21
+        jac[2*N-1,N-2] = (1/(2*h))*self.Gxp(solmat[N-1],solmat[2*N-1],
+                            (BETA[0] - solmat[N-2])/(2*h),(BETA[1] - solmat[2*N-2])/(2*h))
+        jac[2*N-1,N-1] = -self.Gx(solmat[N-1],solmat[2*N-1],
+                            (BETA[0] - solmat[N-2])/(2*h),(BETA[1] - solmat[2*N-2])/(2*h))
+
+        # Populate final values of M12
+        jac[N-1,2*N-2] = (1/(2*h))*self.Fyp(solmat[N-1],solmat[2*N-1],
+                            (BETA[0] - solmat[N-2])/(2*h),(BETA[1] - solmat[2*N-2])/(2*h))
+        jac[N-1,2*N-1] = -self.Fy(solmat[N-1],solmat[2*N-1],
+                            (BETA[0] - solmat[N-2])/(2*h),(BETA[1] - solmat[2*N-2])/(2*h))
+
+        # Populate final values of M22
+        jac[2*N-1,2*N-2] = 1/h**2 + (1/(2*h))*self.Gyp(solmat[N-1],solmat[2*N-1],
+                            (BETA[0] - solmat[N-2])/(2*h),(BETA[1] - solmat[2*N-2])/(2*h))
+        jac[2*N-1,2*N-1] = -2/h**2 - self.Gy(solmat[N-1],solmat[2*N-1],
+                            (BETA[0] - solmat[N-2])/(2*h),(BETA[1] - solmat[2*N-2])/(2*h))
+
+        return jac
